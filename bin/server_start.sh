6c8fa9114085dbc2b357989811a98fbd5e56fcf8
alonl <alonlavi@gmail.com>
2015-06-30 15:40:16 +0300

    docker

--- bin/server_start.sh
+++ bin/server_start.sh
@@ -78,16 +78,17 @@ fi
 export SPARK_HOME
 
 
-RUN = "$SPARK_HOME/bin/spark-submit --class $MAIN --driver-memory $DRIVER_MEMORY \
-           --conf \"spark.executor.extraJavaOptions=$LOGGING_OPTS\" \
-           --driver-java-options \"$GC_OPTS $JAVA_OPTS $LOGGING_OPTS $CONFIG_OVERRIDES\" \
+RUN="$SPARK_HOME/bin/spark-submit --class $MAIN --driver-memory $DRIVER_MEMORY
+           --conf \"spark.executor.extraJavaOptions=$LOGGING_OPTS\"
+           --driver-java-options \"$GC_OPTS $JAVA_OPTS $LOGGING_OPTS $CONFIG_OVERRIDES\"
            $@ $appdir/spark-job-server.jar $conffile 2>&1"
 
-if [ "$1" = "attached" ]; then
+if [ "$ATTACHED" = true ]; then
   echo "Running in attached mode..."
-  `$RUN`
+  eval $RUN
 else
   echo "Running in detached mode..."
-  `$RUN` &
+  RUN="$RUN &"
+  eval $RUN
   echo $! > $pidFilePath
 fi
